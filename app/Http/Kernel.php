ecde7f7e063b2e6c10b032bd07b14af782532342
ahmadrasyidsalim <github.rasyid@gmail.com>
2018-05-09 14:56:47 +0700

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,7 +58,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'jwt.auth' => 'Tymon\JWTAuth\Middleware\GetUserFromToken',
-	    'jwt.refresh' => 'Tymon\JWTAuth\Middleware\RefreshToken',
+	'jwt.auth' => Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+	'jwt.refresh' => Tymon\JWTAuth\Middleware\RefreshToken::class,
     ];
 }
